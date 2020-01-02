903ef6aa1ac1633d449dad581305943acc11e4c9
John B <john@nexys.ch>
2020-01-02 11:01:52 +0100

    added babel config

---
+++ babel.config.js
@@ -0,0 +1,22 @@
+// babel.config.js
+const plugins = [
+    '@babel/plugin-proposal-class-properties',
+    '@babel/plugin-transform-modules-commonjs',
+    '@babel/plugin-proposal-object-rest-spread',
+    '@babel/plugin-transform-async-to-generator',
+    '@babel/plugin-transform-arrow-functions'
+  ]
+
+module.exports = {
+  presets: [
+    [
+      '@babel/preset-env',
+      {
+        targets: {
+          node: 'current',
+        },
+      },
+    ],
+  ],
+  plugins,
+};
