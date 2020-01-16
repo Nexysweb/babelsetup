1b5621923fc334c946cf375fa2299cb6c3ea3a23
John B <johan@nexys.ch>
2020-01-16 10:49:09 +0100

    Update babel.config.js

--- babel.config.js
+++ babel.config.js
@@ -1,22 +1,23 @@
 // babel.config.js
 const plugins = [
-    '@babel/plugin-proposal-class-properties',
-    '@babel/plugin-transform-modules-commonjs',
-    '@babel/plugin-proposal-object-rest-spread',
-    '@babel/plugin-transform-async-to-generator',
-    '@babel/plugin-transform-arrow-functions'
-  ]
+  '@babel/plugin-proposal-class-properties',
+  '@babel/plugin-transform-modules-commonjs',
+  '@babel/plugin-proposal-object-rest-spread',
+  '@babel/plugin-transform-async-to-generator',
+  '@babel/plugin-transform-arrow-functions',
+  ['@babel/plugin-transform-runtime', {
+    corejs: 3,
+    helpers: true,
+    regenerator: true,
+    useESModules: false
+  }]
+];
+
+const presets = [
+  '@babel/preset-env'
+];
 
 module.exports = {
-  presets: [
-    [
-      '@babel/preset-env',
-      {
-        targets: {
-          node: 'current',
-        },
-      },
-    ],
-  ],
+  presets,
   plugins,
 };
